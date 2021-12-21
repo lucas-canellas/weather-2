ce37a1963d87d9e2afc4898911c598361d24229d
lucas-canellas <lucascanellas91@gmail.com>
2021-12-20 23:44:59 -0300

    link api

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 // https://stackoverflow.com/questions/47076743/cors-anywhere-herokuapp-com-not-working-503-what-else-can-i-try
+// "https://limitless-taiga-95649.herokuapp.com/https://www.metaweather.com/api/",
 
 const api = axios.create({
   baseURL:
-    "https://limitless-taiga-95649.herokuapp.com/https://www.metaweather.com/api/",
+    "https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/",
 
 });
 
